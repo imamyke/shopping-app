0f057f79b9d7b8bf785ff97f47cc4ed656b1ced8
ImAmyKe <xiahamy@gmail.com>
2023-07-24 16:25:25 +0800

    eat: create signupUser

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -33,6 +33,28 @@ const userController = {
       throw new Error('User not found')
     }
   }),
+  signupUser: asyncHandler (async (req, res) => {
+    const { name, email, password } = req.body
+    const userExist = await User.findOne({ email })
+
+    if (userExist) {
+      res.status(400)
+      throw new Error('User already exists')
+    }
+    const user = await User.create({ name, email, password })
+    if (user) {
+      res.status(201).json({
+        _id: user._id,
+        name: user.name,
+        email: user.email,
+        isAdmin: user.isAdmin,
+        token: generateToken(user._id)
+      })
+    } else {
+      res.status(400)
+      throw new Error('Invalid user data')
+    }
+  })
 }
 
 module.exports = userController
--- backend/routes/modules/apiGetUsers.js
+++ backend/routes/modules/apiGetUsers.js
@@ -4,6 +4,7 @@ const userController = require('../../controllers/userController')
 const { protect } = require('../../middlewares/authMiddleware')
 
 router.post('/login', userController.authUser)
+router.post('/signup', userController.signupUser)
 router.get('/profile', protect, userController.getUserProfile)
 
 module.exports = router
