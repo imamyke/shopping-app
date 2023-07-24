41bf15952bbd62112e994ae0e95a6644c5079406
ImAmyKe <xiahamy@gmail.com>
2023-07-24 15:30:21 +0800

    feat: apiGetUser is done

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -19,6 +19,20 @@ const userController = {
       throw new Error('Invalid email or password')
     }
   }),
+  getUserProfile: asyncHandler (async (req, res) => {
+    const user = await User.findById(req.user._id)
+    if (user) {
+      res.json({
+        _id: user._id,
+        name: user.name,
+        email: user.email,
+        isAdmin: user.isAdmin
+      })
+    } else {
+      res.status(404)
+      throw new Error('User not found')
+    }
+  }),
 }
 
 module.exports = userController
--- backend/routes/modules/apiGetUsers.js
+++ backend/routes/modules/apiGetUsers.js
@@ -1,7 +1,9 @@
 const express = require('express')
 const router = express.Router()
 const userController = require('../../controllers/userController')
+const { protect } = require('../../middlewares/authMiddleware')
 
 router.post('/login', userController.authUser)
+router.get('/profile', protect, userController.getUserProfile)
 
 module.exports = router
