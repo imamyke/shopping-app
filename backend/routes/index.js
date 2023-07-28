42144eb90448557b30a8291b46b9b6c85b6075a2
ImAmyKe <xiahamy@gmail.com>
2023-07-28 16:31:46 +0800

    feat: apiGetProduct to get products

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,11 +1,13 @@
 const express = require('express')
 const router = express.Router()
 const apiGetUsers = require('./modules/apiGetUsers')
+const apiGetProducts = require('./modules/apiGetProducts')
 
 const upload = require('./modules/uploadRoutes')
 const path = require('path')
 
 router.use('/api/users', apiGetUsers)
+router.use('/api/products', apiGetProducts)
 router.use('/api/upload', upload)
 
 // const __dirname = path.resolve()
